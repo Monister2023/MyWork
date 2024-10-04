3a7312a51d0da6fdb1deb1328551ae5120ff8429
Myotra <mochniy2889@mail.com>
2024-10-05 01:44:46 +0300

    05/10, 1:44

---
+++ module_2_3.py
@@ -0,0 +1,16 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while index<len(my_list):
+    vvod = int(input('Введите число из списка - 42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5: '))
+    if vvod not in my_list:
+        print('Вы ввели число не из списка!')
+        continue
+    elif vvod in my_list and vvod>=1:
+        print('Число положительное!')
+        index += 1
+        continue
+    elif vvod<1:
+        print('Вы ввели отрецательное число!')
+    break
+
+
