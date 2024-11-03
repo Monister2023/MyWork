ae445e63573090d6a5296262375a800ac9a5f522
Admin <monister2889@yandex.ru>
2024-11-03 15:14:33 +0300

    03/11

--- module_3_2.py
+++ module_3_2.py
@@ -1,5 +1,8 @@
 def send_email(message,recipient,sender = "university.help@gmail.com"):
-    if "@" not in (sender and recipient) or not sender.endswith((".com", ".ru", ".net")) and not recipient.endswith((".com", ".ru", ".net")) :
+    if "@" not in sender or not sender.endswith((".com", ".ru", ".net")):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+        return
+    if "@" not in recipient or not recipient.endswith((".com", ".ru", ".net")):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
         return
     if  sender == recipient:
@@ -18,3 +21,5 @@ send_email('Ты победитель!','sensivity@.com')
 send_email('Купи хлеба!','university.help@gmail.com')
 send_email('Какие условия для переговоров?','combat_76@bk.ru','minimal_66@gmail.net')
 send_email('Прожыви достойно!!!','lordfilm.ru')
+send_email('Засада в третьем корпусе!','urban.teacher@mai.uk', 'urban.student@mail.ru')
+
